2c1a2c87168b40f978d929bb7f617f50627711b3
joestack <joern@joern-online.de>
2023-05-31 10:35:17 +0200

    2.2

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,12 @@ output "dns_name" {
   value       = module.boundary.dns_name
 }
 
+output "s3command" {
+  description = "The S3 cp command used to display the contents of the cloud-init-output.log"
+
+  value = format(
+    "aws s3 cp s3://%s/%s -",
+    aws_s3_bucket.boundary.id,
+    data.aws_s3_bucket_objects.cloudinit.keys[0]
+  )
+}
