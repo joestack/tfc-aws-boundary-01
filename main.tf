3e32d910ed7587ae851cee4aa4de453c132530b1
joestack <joern@joern-online.de>
2023-06-02 15:11:24 +0200

    1.4.1

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 module "boundary" {
   source  = "app.terraform.io/JoeStack/boundary/aws"
-  version = "1.4.0"
+  version = "1.4.1"
 
   key_name = "joestack"
 }
--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "dns_name" {
   description = "The public DNS name of the controller load balancer"
   value       = module.boundary.dns_name
 }
+output "dns_name_set" {
+  description = "The public DNS name of the controller load balancer"
+  value       = module.boundary.dns_name_set
+}
 
 # output "s3command" {
 #   description = "The S3 cp command used to display the contents of the cloud-init-output.log"
